bc705388cc919ff08b13704aefb21f3294097f82
Ali <ali@ali.ali.com>
2025-06-19 20:36:00 +0500

    set seo

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://axoraweb.vercel.app',
+  generateRobotsTxt: true,
+  generateIndexSitemap: true,
+  sitemapSize: 5000,
+  changefreq: 'weekly',
+  priority: 0.7,
+};
